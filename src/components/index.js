c8852c7a3185df1065badbdad02694af48658f87
leticia.vargas <le.mvargas@gmail.com>
2023-02-15 20:22:10 -0300

    Create and update components index

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import Card from "./Card";
+import Header from "./Header";
+import Icon from "./Icon";
+import Section from "./Section";
+import FilterMenu from "./FilterMenu";
+
+import SearchInput from "./Inputs/SearchInput";
+import Checkbox from "./Inputs/Checkbox";
+
+export {
+  Card,
+  Header, 
+  Icon,
+  Section,
+  FilterMenu,
+  SearchInput,
+  Checkbox,
+}
