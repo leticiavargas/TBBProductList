1d1891a8717689da626b4ad09717869e9ccbc9a5
leticia.vargas <le.mvargas@gmail.com>
2023-02-15 16:21:38 -0300

    add fontawesome

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
 import './index.scss';
 import App from './App';
+
+library.add(fas, far);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
