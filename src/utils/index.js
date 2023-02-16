303e02b9efb9a515452724dae9bb46f8a13d8917
leticia.vargas <le.mvargas@gmail.com>
2023-02-16 19:20:43 -0300

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,16 +1,8 @@
 export function FilterProductsByCategory (products, categories) {
   if (categories.length === 0) return products;
-  return products?.filter((product) => {
-    if (categories.includes(product.category._id) ) {
-      return product;
-    }
-  })
+  return products?.filter((product) => categories.includes(product.category._id))
 };
 
 export function FilterProductsByName (products, searchQuery) {
-  return products.filter((product) => {
-    if (product.name.toLowerCase().includes(searchQuery.toLowerCase())) {
-      return product;
-    }
-  })
+  return products.filter((product) => product.name.toLowerCase().includes(searchQuery.toLowerCase()))
 }
